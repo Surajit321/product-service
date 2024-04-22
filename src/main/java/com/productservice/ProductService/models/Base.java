fc1a9b61f17cd266767699db7bc255ede05fe1f6
Surajit Borah <surajitsb.borah@gmail.com>
2024-04-23 00:04:28 +0530

    Added DB connectivity

--- src/main/java/com/productservice/ProductService/models/Base.java
+++ src/main/java/com/productservice/ProductService/models/Base.java
@@ -1,10 +1,14 @@
 package com.productservice.ProductService.models;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class Base {
+    @Id
     private long id;
 }
--- src/main/java/com/productservice/ProductService/models/Product.java
+++ src/main/java/com/productservice/ProductService/models/Product.java
@@ -1,13 +1,17 @@
 package com.productservice.ProductService.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends Base {
     private String title;
     private int price;
+    @ManyToOne
     private Category category;
     private String description;
     private String imageUrl;
